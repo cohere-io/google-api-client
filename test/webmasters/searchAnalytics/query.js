61512d2dce7ce2b12bc8aa7a3a992d5dd3e34685
Kurt Freytag <kurt@crunchbase.com>
2016-07-12 21:56:26 -0700

    Added url crawl errors, mcf

--- test/webmasters/searchAnalytics/query.js
+++ test/webmasters/searchAnalytics/query.js
@@ -5,17 +5,19 @@ var refreshToken = require('../../../lib/oauth2/refreshToken'),
     config = require('config');
 
 var queryParams = {
-    startDate: '2016-02-01',
-    endDate: '2016-02-01',
+    startDate: '2016-06-01',
+    endDate: '2016-06-01',
     dimensions: ['date','query', 'page', 'device', 'country'],    
     rowLimit: 5000
 };
 
-refreshToken(config.sites.tld.googleAuth.refreshToken, config.google.clientId, config.google.clientSecret)
+refreshToken(config.sites.glorycycles.googleAuth.refreshToken, config.google.clientId, config.google.clientSecret)
 .then(function(accessToken) {
-    query(config.sites.tld.websiteUrl, queryParams, accessToken, config.google.clientId)
+    query(config.sites.glorycycles.websiteUrl, queryParams, accessToken, config.google.clientId)
     .spread(function(response, body) {
-        console.log(body);
+        Object.keys(body.rows).forEach(function(key) {
+            console.log(body.rows[key]);
+        });
     });
 })
 .catch(function(error) {
